ca6bfedeb540eec8561fb0b2e3da928ceeb554a9
mr-scrpt <mr.scrpt@gmail.com>
2020-04-20 12:58:09 +0300

    arhived to current pathname

--- start.js
+++ start.js
@@ -5,33 +5,106 @@ const { promisify } = require("util");
 const readdir = promisify(fs.readdir);
 const stat = promisify(fs.stat);
 
-const readDir = async (base, archive, exception, dirname = "") => {
+const readDir = async (from, archive) => {
+  const cylceDir = async (start, archive) => {
+    try {
+      let list = await readdir(start);
+
+      for await (const it of list) {
+        const targetPath = path.join(start, it);
+        console.log("-> it", it);
+        const nameForArchive = await pather(targetPath, from);
+        const stats = await stat(targetPath);
+
+        if (stats && stats.isFile()) {
+          archive.append(fs.createReadStream(targetPath), {
+            name: nameForArchive,
+          });
+        } else {
+          await cylceDir(targetPath, archive);
+        }
+      }
+    } catch (err) {}
+  };
+
+  await cylceDir(from, archive);
+};
+
+const zipper = async () => {
+  const from = path.resolve("h:/solutions/node/scanner-to-backup/testfolder");
+  const to = path.resolve("h:/solutions/node/scanner-to-backup/test.zip");
+  const output = fs.createWriteStream(to);
+  const archive = archiver("zip", {
+    zlib: { level: 9 },
+  });
+
+  archive.pipe(output);
+
+  await readDir(from, archive);
+  console.log("-> end");
+  archive.on("error", function (err) {
+    throw err;
+  });
+
+  archive.on("warning", function (err) {
+    if (err.code === "ENOENT") {
+      // log warning
+    } else {
+      // throw error
+      throw err;
+    }
+  });
+
+  output.on("close", function () {
+    console.log(archive.pointer() + " total bytes");
+    console.log(
+      "archiver has been finalized and the output file descriptor has closed."
+    );
+  });
+  output.on("end", function () {
+    console.log("Data has been drained");
+  });
+
+  archive.finalize();
+};
+
+const pather = async (target, subtrahend) => {
+  let res = target.replace(subtrahend, "");
+  if (res[0] === "\\" || "/") {
+    res = res.substr(1, res.length);
+  }
+  return res;
+};
+zipper();
+/* const readDir = async (base, target, archive, exception) => {
   try {
     let list = await readdir(base);
+
     for await (const it of list) {
       const targetPath = path.join(base, it);
-      const fileDestWithOutFolder = path.join(dirname, it);
-
+      console.log("-> dir", base);
       const stats = await stat(targetPath);
-      if (exception.includes(dirname)) return null;
+      //if (exception.includes(dirname)) return null;
+
       if (stats && stats.isFile()) {
         archive.append(fs.createReadStream(targetPath), {
-          name: fileDestWithOutFolder,
+          name: targetPath,
         });
       } else {
-        await readDir(targetPath, archive, exception, it);
+        await readDir(base, targetPath, archive, exception);
       }
     }
   } catch (err) {
     throw new Error(err);
   }
-};
-
-const zipper = async () => {
+}; */
+/* const zipper = async () => {
   const exception = ["node_module", "self-test"];
-
-  const options = { base: "testfolder", dest: "/archive.zip" };
-  var output = fs.createWriteStream(__dirname + options.dest);
+  const from = path.resolve("h:/solutions/node/scanner-to-backup/testfolder");
+  const to = path.resolve("h:/solutions/node/scanner-to-backup/test.zip");
+  await readDir(from, archive, exception);
+  const options = { from, to };
+  const output = fs.createWriteStream(options.to);
   const archive = archiver("zip", {
     zlib: { level: 9 },
   });
@@ -58,9 +131,11 @@ const zipper = async () => {
   });
   output.on("end", function () {
     console.log("Data has been drained");
-  });
-  await readDir(options.base, archive, exception);
+  }); 
+
+  
+
   archive.finalize();
 };
 
-zipper();
+zipper(); */
